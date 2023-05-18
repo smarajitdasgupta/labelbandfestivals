126c12dbe9a5cbff1e651fba6f09d15a40794cad
smarajitWOW <sdasgupta@cogz.woolworths.com.au>
2023-05-18 12:37:27 +1000

    Initial commit: Set up label, band, and festivals project
    Project using the Create React App (CRA) template. The main code file, App.tsx, contains the initial implementation of the components and logic for fetching data

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# labelbandfestivals
-Display sorted data of record labels, bands, festivals appearing in different format via API
+# Labels, Bands, Festivals
+
+This project displays band record labe at the top level, below that it list out all bands under their management, and below that it displays which festivals they've attended, if any. All entries are sorted alphabetically.
+
+## Description
+
+The application fetches data from an API and renders record labels along with their associated bands and festivals. It provides a visual representation of the relationships between record labels, bands, and festivals.
+
+## Installation
+
+To run the project locally, follow these steps:
+
+1. Clone the repository: `git clone https://github.com/smarajitdasgupta/labelbandfestivals.git`
+2. Navigate to the project directory: `cd labelbandfestivals`
+3. Install dependencies: `npm install`
+4. Start the development server: `npm start`
+
+The application will be running at [http://localhost:3000](http://localhost:3000).
+
+## Usage
+
+Upon running the application, you will see the record labels displayed along with their associated bands. Each band is listed under its respective record label, and the festivals they participate in are shown as well.
+
+- If the data is still being fetched, a "Loading..." message will be displayed.
+- If an error occurs during data fetching, an error message will be shown with instructions to refresh the page or try again later.
+- Once the data is successfully fetched, the record labels, bands, and festivals will be rendered.
+
+## Technologies Used
+
+- React.js
+- TypeScript
+- CSS
+
+## API
+
+The project uses an API endpoint to fetch the data:
+
+- [https://eacp.energyaustralia.com.au/codingtest/api/v1/festivals](https://eacp.energyaustralia.com.au/codingtest/api/v1/festivals)
---
+++ src/App.tsx
@@ -0,0 +1,125 @@
+import React, { useState, useEffect } from "react";
+import "./styles.css";
+
+interface Band {
+  name: string;
+  recordLabel: string;
+}
+
+interface Festival {
+  name: string;
+  bands: Band[];
+}
+
+const App: React.FC = () => {
+  const [festivals, setFestivals] = useState<Festival[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+  const errorMsg =
+    "Error fetching data. Please refresh the page or try again in some time.";
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://eacp.energyaustralia.com.au/codingtest/api/v1/festivals"
+        );
+        if (response.ok) {
+          const data: Festival[] = await response.json();
+          setFestivals(data);
+        } else {
+          throw new Error(errorMsg);
+        }
+      } catch (error) {
+        setError(errorMsg);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  // Function to get festivals attended by a band
+  const getFestivalsForBand = (bandName: string) => {
+    const bandFestivals: string[] = [];
+    festivals.forEach((festival) => {
+      const foundBand = festival.bands.find((band) => band.name === bandName);
+      if (foundBand) {
+        bandFestivals.push(festival.name);
+      }
+    });
+    return bandFestivals.sort();
+  };
+
+  // Function to render bands for a record label
+  const renderBandsForRecordLabel = (bands: Band[]) => {
+    const sortedBands = bands.sort((a, b) => a.name.localeCompare(b.name));
+
+    return sortedBands.map((band) => (
+      <div className="band-container" key={band.name}>
+        <div className="band-info">
+          <h3 className="band">{band.name}</h3>
+          <span className="band-badge">Music Band</span>
+        </div>
+        {band.name && (
+          <div className="festivals">
+            {getFestivalsForBand(band.name).map((festival) => (
+              <h4 className="festival" key={`${band.name}-${festival}`} >
+                {festival}
+              </h4>
+            ))}
+          </div>
+        )}
+      </div >
+    ));
+  };
+
+  // Function to render record labels and associated bands
+  const renderRecordLabels = () => {
+    const labelBandsMap: { [label: string]: Band[]; } = {};
+
+    if (festivals) {
+      festivals.forEach((festival) => {
+        festival.bands.forEach((band) => {
+          if (band.recordLabel) {
+            if (!labelBandsMap[band.recordLabel]) {
+              labelBandsMap[band.recordLabel] = [];
+            }
+            labelBandsMap[band.recordLabel].push(band);
+          }
+        });
+      });
+    }
+
+    const sortedLabels = Object.keys(labelBandsMap).sort();
+
+    return sortedLabels.map((label) => (
+      <div className="record-label-container" key={label}>
+        <div className="record-label-panel">
+          <div className="record-label-row">
+            <div className="record-label-info">
+              <h2 className="record-label">{label}</h2>
+              <span className="record-label-badge">Record Label</span>
+            </div>
+            {renderBandsForRecordLabel(labelBandsMap[label])}
+          </div>
+        </div>
+      </div>
+    ));
+  };
+
+  return (
+    <div className="app-container">
+      {loading ? (
+        <div className="loading-message">Loading...</div>
+      ) : error ? (
+        <div className="error-message">{error}</div>
+      ) : (
+        renderRecordLabels()
+      )}
+    </div>
+  );
+};
+
+export default App;
